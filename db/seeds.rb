2e478320539dcd2640d724edf9038e20e7663d7d
Keshav Biswa <keshavbiswa21@gmail.com>
2024-07-04 22:05:37 +0530

    removed roles from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,3 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 #
-Role.create(name: "admin")
-Role.create(name: "user")
-Role.create(name: "owner")
